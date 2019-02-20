893e31936884ee95280d448c80d84e380cb3eac7
Tony Galmiche <tony.galmiche@infosaone.com>
2019-02-20 10:21:07 +0100

    Mise en ligne

---
+++ __manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name'     : 'Modules Odoo 12 pour OVE pour la gestion des EIG et des EI',
+    'version'  : '0.1',
+    'author'   : 'InfoSaône',
+    'category' : 'InfoSaône',
+    'description': """
+Modules Odoo 12 pour OVE pour la gestion des EIG et des EI
+===================================================
+""",
+    'maintainer' : 'InfoSaône',
+    'website'    : 'http://www.infosaone.com',
+    'depends'    : [
+        'base',
+    ],
+    'data' : [
+    ],
+    'installable': True,
+    'application': True,
+}
